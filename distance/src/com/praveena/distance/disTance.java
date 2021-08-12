15bc9d989150bba805f7313cd836f292f5ec9f63
praveenachowdary-png <praveenachowdary57@gmail.com>
2021-08-13 00:25:10 +0530

    day5practiceproblem

--- distance/src/com/praveena/distance/disTance.java
+++ distance/src/com/praveena/distance/disTance.java
@@ -1,14 +1,14 @@
 package com.praveena.distance;
 
+import java.util.Scanner;
+
 public class disTance {
-    public static void getRoots(float a,float b,float c){
-            double delta=Math.pow(b,2)-(4*a*c);
-            System.out.println("delta is "+delta);
-            double root1_of_x=(-b + Math.sqrt(delta))/(2*a);
-            double root2_of_x=(-b - Maclassth.sqrt(delta))/(2*a);
-            System.out.println("root1 of x is "+root1_of_x);
-            System.out.println("root2 of x is "+root2_of_x);
-
-        }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the co-ordinates of x and y");
+        int x=sc.nextInt();
+        int y= sc.nextInt();
+        utility u1=new utility();
+        u1.distancecalc(x,y);
     }
-
+}
