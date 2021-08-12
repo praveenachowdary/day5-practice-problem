c528b51496e2527472f0c7986405c5683bdf4566
praveenachowdary-png <praveenachowdary57@gmail.com>
2021-08-12 20:15:48 +0530

    day5practiceproblem

---
+++ power2/src/com/praveena/pow/utility.java
@@ -0,0 +1,12 @@
+package com.praveena.pow;
+
+public class utility {
+
+        public static void poweroftwo(int n){
+            for(int i=1 ;i<=n;i++){
+                int power=2*i;
+                System.out.println("2 * "+i+" = "+power);
+            }
+        }
+    }
+
