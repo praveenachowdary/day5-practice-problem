019c4552a027844124cef8599edff8606af80bd2
praveenachowdary-png <praveenachowdary57@gmail.com>
2021-08-12 21:47:48 +0530

    day5practiceproblem

---
+++ quotient and remainder/src/com/prav/quorem/quotRemainder.java
@@ -0,0 +1,13 @@
+package com.prav.quorem;
+import java.util.Scanner;
+public class quotRemainder {
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("enter the dividend and divisor");
+            int dividend=sc.nextInt();
+            int divisor=sc.nextInt();
+            utility u1=new utility();
+            u1.quotientRemainder(dividend,divisor);
+        }
+    }
+
