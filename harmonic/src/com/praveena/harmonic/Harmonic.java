c28979346677ad40d06fdeab4ca92b9142a5fac3
praveenachowdary-png <praveenachowdary57@gmail.com>
2021-08-12 20:20:38 +0530

    day5practiceproblem

---
+++ harmonic/src/com/praveena/harmonic/Harmonic.java
@@ -0,0 +1,12 @@
+package com.praveena.harmonic;
+import java.util.Scanner;
+public class Harmonic {
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("enter the number to find the harmonic");
+            int n=sc.nextInt();
+            utility u1=new utility();
+            u1.harmoniccalculation(n);
+        }
+    }
+}
---
+++ harmonic/src/com/praveena/harmonic/utility.java
@@ -0,0 +1,16 @@
+package com.praveena.harmonic;
+
+public class utility {
+  
+        public static void harmoniccalculation(int n) {
+            float harmonic = 1;
+
+            // loop to apply the forumula
+            // Hn = H1 + H2 + H3 ... + Hn-1 + Hn-1 + 1/n
+            for (int i = 2; i <= n; i++) {
+                harmonic += (float)1 / i;
+            }
+            System.out.println("harmonic number is " +harmonic);
+        }
+    }
+}
